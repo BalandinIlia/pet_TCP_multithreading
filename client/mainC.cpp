63d3ac53040e0e458d6f1d1f8ce616860c8f08df
Ilia Balandin <ilia.balandin@student.uva.nl>
2025-10-10 09:04:39 +0200

    progress

--- client/mainC.cpp
+++ client/mainC.cpp
@@ -1,81 +1,14 @@
 #include "winsock2.h"
 #include "iostream"
-#include "thread"
-#include "mutex"
 #include "../networking/messages.h"
 #include "../networking/utils.h"
-
-// mutex controlling console
-std::mutex mutCons;
-
-void sendCycle(SOCKET idSocket)
-{
-    short id = 1;
-    for (;;)
-    {
-        std::cin.get();
-        std::cin.get();
-        mutCons.lock();
-        number num = 0;
-        std::cin >> num;
-        std::cout << std::endl << "Sent request id " << id << " with number " << num << std::endl;
-        mutCons.unlock();
-        std::array<char, 11> mes = MS::serializeRequest(num, id);
-        sendAll(idSocket, mes.data(), 11);
-        id++;
-    }
-}
-
-void receiveCycle(SOCKET idSocket)
-{
-    for (;;)
-    {
-        char c;
-        recvAll(idSocket, &c, 1);
-        const MS::ETypeMes t = MS::decodeType(c);
-        switch (t)
-        {
-        case MS::ETypeMes::eAnsEmpty:
-        {
-            std::array<char, 2> buf;
-            recvAll(idSocket, buf.data(), 2);
-            const short id = MS::deserializeAnsEmpty(buf);
-            mutCons.lock();
-            std::cout << std::endl << "Request with id " << id << ": decomposition impossible" << std::endl;
-            mutCons.unlock();
-            break;
-        }
-        case MS::ETypeMes::eAnsInf:
-        {
-            recvAll(idSocket, &c, 1);
-            const int sz = MS::bufSizeAnsInf(c);
-            std::vector<char> buf;
-            buf.resize(sz);
-            recvAll(idSocket, buf.data(), sz);
-            const std::pair<short, std::vector<number>> ans = MS::deserializeAnsInf(buf);
-            mutCons.lock();
-            std::cout << std::endl << "Request with id " << ans.first << ": decomposition possible:";
-            for (number comp : ans.second)
-                std::cout << comp << " ";
-            std::cout << std::endl;
-            mutCons.unlock();
-            break;
-        }
-        default:
-        {
-            mutCons.lock();
-            std::cout << std::endl << "Incorrect code from server" << std::endl;
-            mutCons.unlock();
-            break;
-        }
-        }
-    }
-}
+#include "runner.h"
 
 int main()
 {
     CWSAConfig conf;
 
+    // Tell the user they are running the client
     std::cout << "Client" << std::endl;
 
     SOCKET idSocket = socket(AF_INET, SOCK_STREAM, 0);
@@ -92,10 +25,8 @@ int main()
     serverAddr.sin_port = htons(port);
     connect(idSocket, reinterpret_cast<sockaddr*>(&serverAddr), sizeof(serverAddr));
 
-    std::thread tSend(sendCycle, idSocket);
-    std::thread tReceive(receiveCycle, idSocket);
-    tSend.join();
-    tReceive.join();
+    CRunner r(idSocket);
+    r.run();
 
     return 0;
 }
---
+++ client/runner.cpp
@@ -0,0 +1,102 @@
+#include "runner.h"
+
+std::mutex CRunner::m_mutCons;
+
+CRunner::CRunner(SOCKET idS) : m_idSocket(idS), m_id(1) {}
+
+void CRunner::run()
+{
+    std::thread ts(sendAdapter, this);
+    std::thread tr(recvAdapter, this);
+    ts.join();
+    tr.join();
+}
+
+void CRunner::send()
+{
+    for (;;)
+    {
+        // 'a' is a signal to receive a number from the user
+        char c = 0;
+        while (c != 'a')
+            c = std::cin.get();
+
+        // Take a number from the user (from console). So here I lock the console mutex to own the console.
+        m_mutCons.lock();
+        std::cout << "Input a number: ";
+        number num = 0;
+        std::cin >> num;
+        std::array<char, 11> mes = MS::serializeRequest(num, m_id);
+        sendAll(m_idSocket, mes.data(), 11);
+        std::cout << std::endl << "Sent request " << num << ". Request id " << m_id << ".";
+        m_mutCons.unlock();
+
+        // Modify the table. So here I lock the table mutex to own the table.
+        m_mutTable.lock();
+        m_table[m_id] = num;
+        m_mutTable.unlock();
+        
+        m_id++;
+    }
+}
+
+void CRunner::receive()
+{
+    for (;;)
+    {
+        char c;
+        recvAll(m_idSocket, &c, 1);
+        const MS::ETypeMes t = MS::decodeType(c);
+        switch (t)
+        {
+        case MS::ETypeMes::eAnsEmpty:
+        {
+            std::array<char, 2> buf;
+            recvAll(m_idSocket, buf.data(), 2);
+            const short id = MS::deserializeAnsEmpty(buf);
+
+            // Here I access the table. While I don't modify the table here, I still have to lock the mutex to
+            // make sure the table is not modified while I am accessing it. If I don't do this, I can get some 
+            // intermediate table state (half-modified).
+            m_mutTable.lock();
+            const number n = m_table[id];
+            m_mutTable.unlock();
+
+            // Show a number from the user (to console). So here I lock the console mutex to own the console.
+            m_mutCons.lock();
+            std::cout << std::endl << "Decomposition of " << n << " is impossible";
+            m_mutCons.unlock();
+            break;
+        }
+        case MS::ETypeMes::eAnsInf:
+        {
+            recvAll(m_idSocket, &c, 1);
+            const int sz = MS::bufSizeAnsInf(c);
+            std::vector<char> buf;
+            buf.resize(sz);
+            recvAll(m_idSocket, buf.data(), sz);
+            const std::pair<short, std::vector<number>> ans = MS::deserializeAnsInf(buf);
+            const short id = ans.first;
+            const std::vector<number>& aComp = ans.second;
+
+            m_mutTable.lock();
+            const number n = m_table[id];
+            m_mutTable.unlock();
+
+            m_mutCons.lock();
+            std::cout << std::endl << n << " is decomposable:";
+            for (const number& comp : aComp)
+                std::cout << comp << " ";
+            m_mutCons.unlock();
+            break;
+        }
+        default:
+        {
+            m_mutCons.lock();
+            std::cout << std::endl << "Incorrect code from server";
+            m_mutCons.unlock();
+            break;
+        }
+        }
+    }
+}
